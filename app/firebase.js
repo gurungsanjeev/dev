1d42a0c57fecf9f4aca4bd1686549600a64a639f
LaGarau <lagarauofficial@gmail.com>
2025-10-14 18:26:57 +0545

    Create firebase.js
    Added an external file for firebase's Api handling

---
+++ app/firebase.js
@@ -0,0 +1,16 @@
+// app/firebase.js or lib/firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database"; // for Realtime Database
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBA3jIanOQD9gD9JKXkfuH761ZXj69-ly8",
+  authDomain: "ghumante-yuwa-demo.firebaseapp.com",
+  databaseURL: "https://ghumante-yuwa-demo-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ghumante-yuwa-demo",
+  storageBucket: "ghumante-yuwa-demo.firebasestorage.app",
+  messagingSenderId: "611741769691",
+  appId: "1:611741769691:web:d9b9f371518819fbb211fe"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app); // export the database instance
